e963c7150fdb61a57130a94b9065200bf6d402be
Sumanth <sumanthreddy.katasani@beehyv.com>
2024-08-07 18:45:19 +0530

    new

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-p=12;
+x = parseInt(Math.random()*20.99)
 
-function sum(a,b,c){return a+b+c;}
-
-function dsf(fn){
-    function fn1(a){
-        function fn2(b){
-            function fn3(c){
-                return fn(a,b,c)
-            }
-            return fn3
-        }
-        return fn2
-    }
-    return fn1
-}
-console.log(dsf(sum)(4)(10)(45))
-// document.getElementById("demo").innerHTML = dsf(16);
+p = document.getElementsByTagName('input')[0].value
+document.getElementById("demo").innerHTML = `<h1>GAME</h1><h2>Guess a Number Between 0 and 20</h2>{p}`;
